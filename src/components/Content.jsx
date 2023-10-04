8a52ccf28e0e85bf4cbf925616e8de596b3183b3
Alme <josealejandrogilmendez08@gmail.com>
2023-10-04 10:39:13 -0400

    Add components and pass function
    Add Education and Experience components and pass handleExpand function

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import PersonalInfo from "./CvForm/PersonalInfo";
+import Education from "./CvForm/Education";
+import Experience from "./CvForm/Experience";
 
 function Content(props){
+    const [expand, setExpand] = useState('education')
     const {personalInfo, handleChange} = props;
+
+    const handleExpand = (e) =>{
+        e.target.id === expand ? setExpand(null) :
+        e.target.id === 'education' ? setExpand('education') : setExpand('experience')
+    }
     return(
-        <PersonalInfo personalInfo={personalInfo} handleChange={handleChange}/>
+        <>
+            <PersonalInfo personalInfo={personalInfo} handleChange={handleChange}/>
+            <Education handleExpand={handleExpand} isExpand={expand === 'education'}/>
+            <Experience handleExpand={handleExpand} isExpand={expand === 'experience'}/>
+        </>
     )
 }
 
