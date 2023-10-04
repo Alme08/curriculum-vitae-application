daac05c8580f580b980ab0d98d5146b7e473af93
Alme <josealejandrogilmendez08@gmail.com>
2023-10-04 09:49:10 -0400

    Update components to add type attribute

--- src/components/CvForm/PersonalInfo.jsx
+++ src/components/CvForm/PersonalInfo.jsx
@@ -5,16 +5,15 @@ function PersonalInfo({personalInfo, handleChange}) {
     return(
         <div className="personalInfo">
             <h2>Personal Details</h2>
-            <Input name='fullName' placeholder='Insert full name' onChange={handleChange} id='fullName' value={personalInfo.fullName} 
-            label='Full name'/>
-            <Input name='title' placeholder='Insert title' onChange={handleChange} id='title' value={personalInfo.title} 
-            label='Title' span='recommended'/>
-            <Input name='email' placeholder='Insert email' onChange={handleChange} id='email' value={personalInfo.email} 
-            label='Email' span='recommended'/>
-            <Input name='address' placeholder='City, Country' onChange={handleChange} id='address' value={personalInfo.address} 
-            label='Address' span='recommended'/>
-            <Input name='phone' placeholder='Insert phone number' onChange={handleChange} id='phone' value={personalInfo.phone} 
-            label='phoneNumber' span='recommended'/>
+            <Input type='text' name='fullName' placeholder='Insert full name' onChange={handleChange} id='fullName' value={personalInfo.fullName} label='Full name'/>
+
+            <Input type='text' name='title' placeholder='Insert title' onChange={handleChange} id='title' value={personalInfo.title} label='Title' span='recommended'/>
+
+            <Input type='email' name='email' placeholder='Insert email' onChange={handleChange} id='email' value={personalInfo.email} label='Email' span='recommended'/>
+
+            <Input type='text' name='address' placeholder='City, Country' onChange={handleChange} id='address' value={personalInfo.address} label='Address' span='recommended'/>
+            
+            <Input tpye='tel' name='phone' placeholder='Insert phone number' onChange={handleChange} id='phone' value={personalInfo.phone} label='phoneNumber' span='recommended'/>
         </div>
     )
 }
