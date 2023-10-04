c9398a59ad95100274aee6eb3e7d0c864315e054
Alme <josealejandrogilmendez08@gmail.com>
2023-10-04 09:48:23 -0400

    Add Type attribute

--- src/components/tools/Input.jsx
+++ src/components/tools/Input.jsx
@@ -1,8 +1,8 @@
-function Input({name, placeholder, onChange, id, value, label, span}){
+function Input({type, name, placeholder, onChange, id, value, label, span}){
     return(
         <>
         <label htmlFor={name}>{label}</label><span>{span}</span>
-        <input type="text" name={name} id={id} placeholder={placeholder} onChange={onChange} value={value}/>
+        <input type={type} name={name} id={id} placeholder={placeholder} onChange={onChange} value={value}/>
         </>
     )
 }
