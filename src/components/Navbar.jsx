973a5ff9eccef580ab8bd316d336aef094f181a7
Alme <josealejandrogilmendez08@gmail.com>
2023-10-05 16:23:41 -0400

    Delete logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,6 @@ function Navbar() {
     return(
         <div className="navbar-container">
             <nav className="navbar container">
-                <h1>CV Creator</h1>
                 <ul>
                     <li><a href="https://github.com/Alme08/">View Code</a></li>
                 </ul>
