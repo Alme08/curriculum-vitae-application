d3cf88f41fd0ce80ed109e8e63425f6452fc245a
Alme <josealejandrogilmendez08@gmail.com>
2023-10-05 11:06:30 -0400

    Add div and classNames

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 function Navbar() {
     return(
-        <nav>
-            <h1>CV Creator</h1>
-            <ul>
-                <li><a href="https://github.com/Alme08/">View Code</a></li>
-            </ul>
-        </nav>
+        <div className="navbar-container">
+            <nav className="navbar container">
+                <h1>CV Creator</h1>
+                <ul>
+                    <li><a href="https://github.com/Alme08/">View Code</a></li>
+                </ul>
+            </nav>
+        </div>
     )
 }
 
