277c5639fff34739b97a8fdad1fd70217db02baf
Alme <josealejandrogilmendez08@gmail.com>
2023-10-05 21:22:18 -0400

    Update input components

--- src/components/CvForm/PersonalInfo.jsx
+++ src/components/CvForm/PersonalInfo.jsx
@@ -3,17 +3,19 @@ import Input from "../tools/Input";
 function PersonalInfo({personalInfo, handleChange}) {
     
     return(
-        <div className="personalInfo">
+        <div className="personal-info">
             <h2>Personal Details</h2>
-            <Input type='text' name='fullName' placeholder='Insert full name' onChange={handleChange} id='fullName' value={personalInfo.fullName} label='Full name'/>
+            <form action="">
+                <Input type='text' name='fullName' placeholder='Insert full name' onChange={handleChange} id='fullName' value={personalInfo.fullName} label='Full name' dataset='personalInfo'/>
 
-            <Input type='text' name='title' placeholder='Insert title' onChange={handleChange} id='title' value={personalInfo.title} label='Title' span='recommended'/>
+                <Input type='text' name='title' placeholder='Insert title' onChange={handleChange} id='title' value={personalInfo.title} label='Title' span='recommended' dataset='personalInfo'/>
 
-            <Input type='email' name='email' placeholder='Insert email' onChange={handleChange} id='email' value={personalInfo.email} label='Email' span='recommended'/>
+                <Input type='email' name='email' placeholder='Insert email' onChange={handleChange} id='email' value={personalInfo.email} label='Email' span='recommended' dataset='personalInfo'/>
 
-            <Input type='text' name='address' placeholder='City, Country' onChange={handleChange} id='address' value={personalInfo.address} label='Address' span='recommended'/>
-            
-            <Input tpye='tel' name='phone' placeholder='Insert phone number' onChange={handleChange} id='phone' value={personalInfo.phone} label='phoneNumber' span='recommended'/>
+                <Input type='text' name='address' placeholder='City, Country' onChange={handleChange} id='address' value={personalInfo.address} label='Address' span='recommended' dataset='personalInfo'/>
+                
+                <Input tpye='tel' name='phone' placeholder='Insert phone number' onChange={handleChange} id='phone' value={personalInfo.phone} label='phoneNumber' span='recommended' dataset='personalInfo'/>
+            </form>
         </div>
     )
 }
