bea6fdfde62edeb801a4c4282beb600cba7ae2ae
Alme <josealejandrogilmendez08@gmail.com>
2023-10-07 19:24:07 -0400

    Pass expand state and handleExpand to Main

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,16 +1,13 @@
-import { useState } from "react";
+// import { useState } from "react";
 import PersonalInfo from "./CvForm/PersonalInfo";
 import Education from "./CvForm/Education";
 import Experience from "./CvForm/Experience";
 
 function Content(props){
-    const [expand, setExpand] = useState(null)
-    const {personalInfo, educations, handleChange, handleAddEducation} = props;
+    
+    const {personalInfo, educations, handleChange, handleAddEducation, handleExpand, expand} = props;
 
-    const handleExpand = (e) =>{
-        e.target.id === expand ? setExpand(null) :
-        e.target.id === 'education' ? setExpand('education') : setExpand('experience')
-    }
+    
     return(
         <>
             <PersonalInfo personalInfo={personalInfo} handleChange={handleChange}/>
