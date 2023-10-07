4f831b35bc0a1f85da8e340a2b7ec1ed683cc9e0
Alme <josealejandrogilmendez08@gmail.com>
2023-10-07 19:22:11 -0400

    Fix error
    clicking on the icon didn't trigger anything, fixed by adding the same id as the button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 function Sidebar({handleSidebar, customize}) {
     return(
         <div className="sidebar">
-            <button className={!customize ? "selected" : ''} id="content" onClick={handleSidebar}><i className="fa-solid fa-pen"></i>Content</button>
+            <button className={!customize ? "selected" : ''} id="content" onClick={handleSidebar}><i id="content" className="fa-solid fa-pen"></i>Content</button>
             <button className={customize ? "selected" : ''} id="layout" onClick={handleSidebar}><i className="fa-regular fa-file"></i>Layout</button>
         </div>
     )
