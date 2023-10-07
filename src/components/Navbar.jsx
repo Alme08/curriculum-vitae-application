baaf615cf8f519e930260b09a0873e3db0024734
Alme <josealejandrogilmendez08@gmail.com>
2023-10-07 17:02:50 -0400

    add target="_blank"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ function Navbar() {
         <div className="navbar-container">
             <nav className="navbar container">
                 <ul>
-                    <li><a href="https://github.com/Alme08/curriculum-vitae-application">View Code</a></li>
+                    <li><a href="https://github.com/Alme08/curriculum-vitae-application" target="_blank" rel="noreferrer">View Code</a></li>
                 </ul>
             </nav>
         </div>
