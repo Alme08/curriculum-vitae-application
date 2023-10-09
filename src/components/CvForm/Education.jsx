2cef115a87c2fe1ed5d8c137776c745a202a59ca
Alme <josealejandrogilmendez08@gmail.com>
2023-10-09 19:56:54 -0400

    Import input and add form

--- src/components/CvForm/Education.jsx
+++ src/components/CvForm/Education.jsx
@@ -1,4 +1,6 @@
-function Education({educations, handleExpand, isExpand, handleAddEducation}) {
+import Input from "../tools/Input";
+
+function Education({educations, handleExpand, isExpand, handleAddEducation, handleChange}) {
     const edit = educations.find(education => education.edit === true);
     return(
         <div>
@@ -7,10 +9,18 @@ function Education({educations, handleExpand, isExpand, handleAddEducation}) {
             {isExpand && 
             <div className="sectionOpen">                
                 {edit ? 
-                    <div key={edit.id}>
-                        <p>1</p>
-                        <p>2</p>
-                        <p>3</p>
+                    <div data-id={edit.id} key={edit.id}>
+                        <Input type='text' name='university' placeholder='Insert School / University' onChange={handleChange} id='university' value={edit.university} label='School' dataset='education'/>
+
+                        <Input type='text' name='degree' placeholder='Insert Degree' onChange={handleChange} id='degree' value={edit.degree} label='Degree' dataset='education'/>
+                        
+                        <div data-id={edit.id}>
+                            <Input type='text' name='from' placeholder='Insert Start Date' onChange={handleChange} id='from' value={edit.from} label='Start Date' dataset='education'/>
+
+                            <Input type='text' name='to' placeholder='Insert End Date' onChange={handleChange} id='to' value={edit.to} label='End Date' dataset='education'/>
+                        </div>
+
+                        <Input type='text' name='city' placeholder='Insert Location' onChange={handleChange} id='city' value={edit.city} label='Location' span='optional' dataset='education'/>
                         <div>
                             <button>Delete</button>
                             <button>Cancel</button>
