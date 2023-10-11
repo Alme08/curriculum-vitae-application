41a731092c6e2e30d37092fba16c631bf0db5fcf
Alme <josealejandrogilmendez08@gmail.com>
2023-10-11 15:18:20 -0400

    add div

--- src/components/CvTemplate/MainTemplate.jsx
+++ src/components/CvTemplate/MainTemplate.jsx
@@ -1,34 +1,34 @@
 function MainTemplate({educations, experiences}) {
     return(
-        <>
-        {educations.map((education) => {
-            if (education.hidden === false) {
-                return(
-                    <div key={education.id}>
-                        <p>{education.university}</p>
-                        <p>{education.degree}</p>
-                        <p>{education.from}</p>
-                        <p>{education.to}</p>
-                        <p>{education.city}</p>
-                    </div>
-                )
-            }
-        })}
-        {experiences.map((experience) => {
-            if (experience.hidden === false) {
-                return(
-                    <div key={experience.id}>
-                        <p>{experience.company}</p>
-                        <p>{experience.position}</p>
-                        <p>{experience.from}</p>
-                        <p>{experience.to}</p>
-                        <p>{experience.city}</p>
-                        <p>{experience.description}</p>
-                    </div>
-                )
-            }
-        })}
-        </>
+        <div>
+            {educations.map((education) => {
+                if (education.hidden === false) {
+                    return(
+                        <div key={education.id}>
+                            <p>{education.university}</p>
+                            <p>{education.degree}</p>
+                            <p>{education.from}</p>
+                            <p>{education.to}</p>
+                            <p>{education.city}</p>
+                        </div>
+                    )
+                }
+            })}
+            {experiences.map((experience) => {
+                if (experience.hidden === false) {
+                    return(
+                        <div key={experience.id}>
+                            <p>{experience.company}</p>
+                            <p>{experience.position}</p>
+                            <p>{experience.from}</p>
+                            <p>{experience.to}</p>
+                            <p>{experience.city}</p>
+                            <p>{experience.description}</p>
+                        </div>
+                    )
+                }
+            })}
+        </div>
     )
 }
 
