68594c0478fd2328a91a68cbed6589126c27db38
Alme <josealejandrogilmendez08@gmail.com>
2023-10-11 15:17:37 -0400

    Add classnames

--- src/components/CvTemplate/HeaderTemplate.jsx
+++ src/components/CvTemplate/HeaderTemplate.jsx
@@ -1,14 +1,16 @@
 function HeaderTemplate({personalInfo}){
     return(
-        <>
-            <h2>{personalInfo.fullName}</h2>
-            <p>{personalInfo.title}</p>
-            <div>
-                {personalInfo.email && <p>. {personalInfo.email}</p>}
-                {personalInfo.phone && <p>+ {personalInfo.phone}</p>}
-                {personalInfo.address && <p>* {personalInfo.address}</p>}
+        <header className="headerTemplate">
+            <div className="name">
+                <h2>{personalInfo.fullName}</h2>
+                <p>{personalInfo.title}</p>
             </div>
-        </>
+            <div className="contact">
+                {personalInfo.email && <div><i className="fa-solid fa-envelope"></i> {personalInfo.email}</div>}
+                {personalInfo.phone && <div><i className="fa-solid fa-phone"></i> {personalInfo.phone}</div>}
+                {personalInfo.address && <div><i className="fa-solid fa-location-dot"></i> {personalInfo.address}</div>}
+            </div>
+        </header>
     )
 }
 
